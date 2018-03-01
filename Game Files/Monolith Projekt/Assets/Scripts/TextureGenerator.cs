1e90a5bb108947bdb120e52dfbb24665679e7c79
Cadosphere <36941164+Cadosphere@users.noreply.github.com>
2018-03-01 15:27:49 -0500

    Map Generator Color
    Mostly implemented colors onto the map generator.

---
+++ Game Files/Monolith Projekt/Assets/Scripts/TextureGenerator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class TextureGenerator
+{
+    public static Texture2D TextureFromColorMap(Color[] colorMap, int width, int height)
+    {
+        Texture2D texture = new Texture2D(width, height);
+        texture.SetPixels(colorMap);
+        texture.Apply;
+        return;
+    }
+
+
+}
+
