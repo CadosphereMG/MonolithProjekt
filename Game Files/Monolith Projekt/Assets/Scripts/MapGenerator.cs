3ed989d26c9c8cb2b70a57c0c7af0f06dbb1b4bd
Cadosphere <36941164+Cadosphere@users.noreply.github.com>
2018-03-02 11:55:17 -0500

    Endless Terrain Initial
    Started building the endless terrain engine.

--- Game Files/Monolith Projekt/Assets/Scripts/MapGenerator.cs
+++ Game Files/Monolith Projekt/Assets/Scripts/MapGenerator.cs
@@ -1,43 +1,47 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
 public class MapGenerator : MonoBehaviour
 {
-    public enum DrawMode {NoiseMap, ColorMap}
+
+    public enum DrawMode { NoiseMap, colorMap, Mesh };
     public DrawMode drawMode;
 
-    public int mapWidth;
-    public int mapHeight;
+    public const int mapChunkSize = 241;
+    [Range(0,6)]
+    public int levelOfDetail;
     public float noiseScale;
 
-
     public int octaves;
+    [Range(0, 1)]
     public float persistance;
     public float lacunarity;
 
     public int seed;
     public Vector2 offset;
 
+    public float meshHeightMultiplier;
+    public AnimationCurve meshHeightCurve;
+
     public bool autoUpdate;
 
-    public TerrainType[] regions;   
+    public TerrainType[] regions;
 
     public void GenerateMap()
     {
-        float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, seed, (int)noiseScale, octaves, persistance, lacunarity, offset);
+        float[,] noiseMap = Noise.GenerateNoiseMap(mapChunkSize, mapChunkSize, seed, noiseScale, octaves, persistance, lacunarity, offset);
 
-        Color[] colorMap = new Color[mapWidth * mapHeight];
-        for (int y = 0; y < mapHeight; y++)
+        Color[] colorMap = new Color[mapChunkSize * mapChunkSize];
+        for (int y = 0; y < mapChunkSize; y++)
         {
-            for (int x = 0; x < mapWidth; x++)
+            for (int x = 0; x < mapChunkSize; x++)
             {
-                float currrentHeight = noiseMap[x, y];
+                float currentHeight = noiseMap[x, y];
                 for (int i = 0; i < regions.Length; i++)
                 {
-                    if (currrentHeight <= regions[i].height)
+                    if (currentHeight <= regions[i].height)
                     {
-                        colorMap[y * mapWidth + x] = regions[i].color;
+                        colorMap[y * mapChunkSize + x] = regions[i].color;
                         break;
                     }
                 }
@@ -47,24 +51,20 @@ public class MapGenerator : MonoBehaviour
         MapDisplay display = FindObjectOfType<MapDisplay>();
         if (drawMode == DrawMode.NoiseMap)
         {
-            display.DrawNoiseMap(noiseMap);
+            display.DrawTexture(TextureGenerator.TextureFromHeightMap(noiseMap));
         }
-        else if (drawMode == DrawMode.ColorMap)
+        else if (drawMode == DrawMode.colorMap)
         {
-            display.DrawNoiseMap(colorMap);
+            display.DrawTexture(TextureGenerator.TextureFromcolorMap(colorMap, mapChunkSize, mapChunkSize));
+        }
+        else if (drawMode == DrawMode.Mesh)
+        {
+            display.DrawMesh(MeshGenerator.GenerateTerrainMesh(noiseMap, meshHeightMultiplier, meshHeightCurve, levelOfDetail), TextureGenerator.TextureFromcolorMap(colorMap, mapChunkSize, mapChunkSize));
         }
     }
 
-    private void OnValidate()
+    void OnValidate()
     {
-        if (mapWidth < 1)
-        {
-            mapWidth = 1;
-        }
-        if (mapHeight < 1)
-        {
-            mapHeight = 1;
-        }
         if (lacunarity < 1)
         {
             lacunarity = 1;
@@ -82,5 +82,4 @@ public struct TerrainType
     public string name;
     public float height;
     public Color color;
-
 }
--- Game Files/Monolith Projekt/Assets/Scripts/Noise.cs
+++ Game Files/Monolith Projekt/Assets/Scripts/Noise.cs
@@ -1,19 +1,19 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
-public class Noise
+public static class Noise
 {
-    public static float[,] GenerateNoiseMap(int mapWidth, int seed, int mapHeight, int octaves, float scale, float persistance, float lacunarity, Vector2 offset)
+
+    public static float[,] GenerateNoiseMap(int mapWidth, int mapHeight, int seed, float scale, int octaves, float persistance, float lacunarity, Vector2 offset)
     {
         float[,] noiseMap = new float[mapWidth, mapHeight];
 
-        System.Random SeedUsed = new System.Random(seed);
+        System.Random prng = new System.Random(seed);
         Vector2[] octaveOffsets = new Vector2[octaves];
         for (int i = 0; i < octaves; i++)
         {
-            float offsetX = SeedUsed.Next(-100000, 100000) + offset.x;
-            float offsetY = SeedUsed.Next(-100000, 100000) + offset.y;
+            float offsetX = prng.Next(-100000, 100000) + offset.x;
+            float offsetY = prng.Next(-100000, 100000) + offset.y;
             octaveOffsets[i] = new Vector2(offsetX, offsetY);
         }
 
@@ -25,18 +25,23 @@ public class Noise
         float maxNoiseHeight = float.MinValue;
         float minNoiseHeight = float.MaxValue;
 
+        float halfWidth = mapWidth / 2f;
+        float halfHeight = mapHeight / 2f;
+
+
         for (int y = 0; y < mapHeight; y++)
         {
             for (int x = 0; x < mapWidth; x++)
             {
+
                 float amplitude = 1;
                 float frequency = 1;
                 float noiseHeight = 0;
 
                 for (int i = 0; i < octaves; i++)
                 {
-                    float sampleX = x / scale * frequency + octaveOffsets[i].x;
-                    float sampleY = y / scale * frequency + octaveOffsets[i].y;
+                    float sampleX = (x - halfWidth) / scale * frequency + octaveOffsets[i].x;
+                    float sampleY = (y - halfHeight) / scale * frequency + octaveOffsets[i].y;
 
                     float perlinValue = Mathf.PerlinNoise(sampleX, sampleY) * 2 - 1;
                     noiseHeight += perlinValue * amplitude;
@@ -44,6 +49,7 @@ public class Noise
                     amplitude *= persistance;
                     frequency *= lacunarity;
                 }
+
                 if (noiseHeight > maxNoiseHeight)
                 {
                     maxNoiseHeight = noiseHeight;
@@ -55,6 +61,7 @@ public class Noise
                 noiseMap[x, y] = noiseHeight;
             }
         }
+
         for (int y = 0; y < mapHeight; y++)
         {
             for (int x = 0; x < mapWidth; x++)
@@ -65,4 +72,5 @@ public class Noise
 
         return noiseMap;
     }
+
 }
